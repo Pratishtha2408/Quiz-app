43daaa4f9f755afb375a6128f0d4ad3f05df6bcd
Pratishtha2408 <90457548+Pratishtha2408@users.noreply.github.com>
2024-09-03 15:29:37 +0530

    Delete online-quiz directory

--- online-quiz/src/main/java/com/pdhiraj/online_quiz/repository/QuizRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pdhiraj.online_quiz.repository;
-
-import com.pdhiraj.online_quiz.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-}
